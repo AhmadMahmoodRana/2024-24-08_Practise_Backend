aeca064cd89787f651afc2af1816a45c6d724257
AhmadMahmoodRana <techexpert10885@gmail.com>
2024-08-24 21:29:35 +0500

    DATA BASE AND SERVER CONNECTION

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,30 @@
 import express from 'express';
 import 'dotenv/config'
+import connectDataBase from './Config/database.js'
+import cors from 'cors'
+import cookieParser  from 'cookie-parser'
+
 const PORT = process.env.PORT || 4000
-
 const app = express();
+
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}))
+app.use(cookieParser())
 
 
-
-
-
-app.listen(PORT,() =>{
-    console.log(`Server is running on port ${PORT}`)  
+connectDataBase().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+    })
 })
+    .catch((error) => {
+        console.error('Database connection error:', error);
+    });
+
+
+
+
 
